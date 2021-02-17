efde2db30ff0ef1cc848dce709d307d0b546a59f
yourfriendyo <3395537299@qq.com>
2021-02-17 16:04:20 +0800

    6665

---
+++ 两数比较/两数比较.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+
+{int a,b;
+printf("输入第一个值：");
+scanf("%d",&a);
+printf("输入第二个值：");
+scanf("%d",&b);
+if(a>b)printf("a大于b\n");
+else printf("a小b\n");
+return 0;
+
+}
